05e837337b4cf3d6b712bc54e4b9237e824f765f
ramzan_sheba <ramzan@sheba.xyz>
2022-07-12 22:21:23 +0600

    note list adapter adds to list fragment

--- app/src/main/java/com/example/simpletodoapp/ui/note_list/NoteListFragment.kt
+++ app/src/main/java/com/example/simpletodoapp/ui/note_list/NoteListFragment.kt
@@ -6,7 +6,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.simpletodoapp.databinding.FragmentListBinding
+import com.example.simpletodoapp.ui.note_list.adapter.NoteListAdapter
 
 
 class NoteListFragment : Fragment(){
@@ -16,6 +19,8 @@ class NoteListFragment : Fragment(){
     private var _binding: FragmentListBinding? = null
     private val binding get() = _binding!!
 
+    private val adapter: NoteListAdapter by lazy { NoteListAdapter() }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,10 +29,18 @@ class NoteListFragment : Fragment(){
         binding.lifecycleOwner = this
 
         noteListViewModel.getAllData.observe(viewLifecycleOwner, Observer {
-            Toast.makeText(requireContext(), it.toString(), Toast.LENGTH_SHORT).show()
+            adapter.setData(it)
         })
 
+        setupRecyclerview()
+
         return binding.root
+    }
+
+    private fun setupRecyclerview() {
+        val recyclerView = binding.rvNoteList
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/simpletodoapp/ui/note_list/adapter/NoteListAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.simpletodoapp.ui.note_list.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.simpletodoapp.databinding.NoteListRowLayoutBinding
+import com.example.simpletodoapp.model.Note
+
+
+class NoteListAdapter : RecyclerView.Adapter<NoteListAdapter.MyViewHolder>() {
+
+    var dataList = emptyList<Note>()
+
+    class MyViewHolder(private val binding: NoteListRowLayoutBinding) : RecyclerView.ViewHolder(binding.root){
+
+        fun bind(Note: Note){
+            binding.note = Note
+            binding.executePendingBindings()
+        }
+        companion object{
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = NoteListRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(
+                    binding
+                )
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(
+            parent
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = dataList[position]
+        holder.bind(currentItem)
+    }
+
+    fun setData(Note: List<Note>){
+        val toDoDiffUtil = NoteDiffUtil(dataList, Note)
+        val toDoDiffResult = DiffUtil.calculateDiff(toDoDiffUtil)
+        this.dataList = Note
+        toDoDiffResult.dispatchUpdatesTo(this)
+    }
+
+}
