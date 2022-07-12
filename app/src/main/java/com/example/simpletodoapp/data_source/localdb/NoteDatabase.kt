c7f3e200f7f4c6ff408c55862f989ae25382e65d
ramzan_sheba <ramzan@sheba.xyz>
2022-07-12 18:49:18 +0600

    note database adds

--- app/src/main/java/com/example/simpletodoapp/data_source/localdb/NoteDatabase.kt
+++ app/src/main/java/com/example/simpletodoapp/data_source/localdb/NoteDatabase.kt
@@ -1,4 +1,32 @@
 package com.example.simpletodoapp.data_source.localdb
 
-class NoteDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.simpletodoapp.model.Note
+import com.example.simpletodoapp.util.Constants
+
+@Database(entities = [Note::class], version = 1, exportSchema = false)
+abstract class NoteDatabase : RoomDatabase() {
+
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        fun getDatabase(context: Context): NoteDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE
+                    ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                NoteDatabase::class.java, Constants.DBConst.DB_NAME
+            ).build()
+    }
+
 }
