b79e94d4f1eff92dea5ec9fd23d96cd1e058e4db
ramzan_sheba <ramzan@sheba.xyz>
2022-07-12 18:39:43 +0600

    dao adds

--- app/src/main/java/com/example/simpletodoapp/data_source/localdb/NoteDao.kt
+++ app/src/main/java/com/example/simpletodoapp/data_source/localdb/NoteDao.kt
@@ -1,4 +1,24 @@
 package com.example.simpletodoapp.data_source.localdb
 
-class NoteDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.simpletodoapp.model.Note
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<Note>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertData(toDoData: Note)
+
+    @Update
+    suspend fun updateData(toDoData: Note)
+
+    @Delete
+    suspend fun deleteItem(toDoData: Note)
+
+    @Query("DELETE FROM note_table")
+    suspend fun deleteAll()
 }
