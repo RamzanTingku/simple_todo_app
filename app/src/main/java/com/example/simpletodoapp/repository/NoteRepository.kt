976d16f048d722ec09876ca7396deba54aad6380
ramzan_sheba <ramzan@sheba.xyz>
2022-07-12 18:53:22 +0600

    note repository adds

--- app/src/main/java/com/example/simpletodoapp/repository/NoteRepository.kt
+++ app/src/main/java/com/example/simpletodoapp/repository/NoteRepository.kt
@@ -1,4 +1,26 @@
 package com.example.simpletodoapp.repository
 
-class NoteRepository {
+import androidx.lifecycle.LiveData
+import com.example.simpletodoapp.data_source.localdb.NoteDao
+import com.example.simpletodoapp.model.Note
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+    val getAllData: LiveData<List<Note>> = noteDao.getAllData()
+
+    suspend fun insertData(toDoData: Note){
+        noteDao.insertData(toDoData)
+    }
+
+    suspend fun updateData(toDoData: Note){
+        noteDao.updateData(toDoData)
+    }
+
+    suspend fun deleteItem(toDoData: Note){
+        noteDao.deleteItem(toDoData)
+    }
+
+    suspend fun deleteAll(){
+        noteDao.deleteAll()
+    }
 }
