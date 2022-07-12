efa9e2ea8eaaa515e7dc75757bc0b766922ff5be
ramzan_sheba <ramzan@sheba.xyz>
2022-07-12 18:35:19 +0600

    entity adds for room

--- app/src/main/java/com/example/simpletodoapp/model/Note.kt
+++ app/src/main/java/com/example/simpletodoapp/model/Note.kt
@@ -1,4 +1,15 @@
 package com.example.simpletodoapp.model
 
-class Note {
-}
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "note_table")
+@Parcelize
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var description: String
+): Parcelable
