a891af59d77e697b39bb92bad46517f33f5948a4
ramzan_sheba <ramzan@sheba.xyz>
2022-07-13 00:11:02 +0600

    note details fragment adds

--- app/src/main/java/com/example/simpletodoapp/ui/BindingAdapters.kt
+++ app/src/main/java/com/example/simpletodoapp/ui/BindingAdapters.kt
@@ -1,8 +1,11 @@
 package com.example.simpletodoapp.ui
 
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.navigation.findNavController
 import com.example.simpletodoapp.R
+import com.example.simpletodoapp.model.Note
+import com.example.simpletodoapp.ui.note_list.NoteListFragmentDirections
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BindingAdapters {
@@ -18,6 +21,16 @@ class BindingAdapters {
                 }
             }
         }
+
+        @BindingAdapter("android:sendDataToNoteDetailFragment")
+        @JvmStatic
+        fun sendDataToNoteDetailFragment(view: ConstraintLayout, note: Note){
+            view.setOnClickListener {
+                val action = NoteListFragmentDirections.actionListFragmentToDetailFragment(note)
+                view.findNavController().navigate(action)
+            }
+        }
+
 
     }
 
