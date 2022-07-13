e96ed30bd7b332f25f3d7dfd8a2406aa3e3d09e2
ramzan_sheba <ramzan@sheba.xyz>
2022-07-13 07:48:56 +0600

    edit view add in progress

--- app/src/main/java/com/example/simpletodoapp/ui/note_detail/NoteDetailsFragment.kt
+++ app/src/main/java/com/example/simpletodoapp/ui/note_detail/NoteDetailsFragment.kt
@@ -1,14 +1,21 @@
 package com.example.simpletodoapp.ui.note_detail
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.*
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.simpletodoapp.R
 import com.example.simpletodoapp.databinding.FragmentNoteDetailsBinding
 
-class NoteDetailsFragment : Fragment() {
+class NoteDetailsFragment : Fragment() , MenuProvider {
 
     private val args by navArgs<NoteDetailsFragmentArgs>()
     private var _binding: FragmentNoteDetailsBinding? = null
@@ -21,11 +28,22 @@ class NoteDetailsFragment : Fragment() {
         _binding = FragmentNoteDetailsBinding.inflate(inflater, container, false)
         binding.note = args.note
 
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.update_fragment_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
