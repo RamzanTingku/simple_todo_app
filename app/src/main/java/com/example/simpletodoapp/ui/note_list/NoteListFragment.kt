e80b36cfd95586a213ee3d15d073884e2da8b37e
ramzan_sheba <ramzan@sheba.xyz>
2022-07-20 01:31:06 +0600

    theming adds

--- app/src/main/java/com/example/simpletodoapp/ui/note_list/NoteListFragment.kt
+++ app/src/main/java/com/example/simpletodoapp/ui/note_list/NoteListFragment.kt
@@ -1,17 +1,24 @@
 package com.example.simpletodoapp.ui.note_list
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.simpletodoapp.R
 import com.example.simpletodoapp.databinding.FragmentListBinding
 import com.example.simpletodoapp.ui.note_list.adapter.NoteListAdapter
+import com.example.simpletodoapp.util.isDarkThemeOn
 
 
-class NoteListFragment : Fragment(){
+class NoteListFragment : Fragment(), MenuProvider {
 
     private val noteListViewModel: NoteListViewModel by viewModels()
 
@@ -26,6 +33,9 @@ class NoteListFragment : Fragment(){
     ): View {
         _binding = FragmentListBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         noteListViewModel.getAllData.observe(viewLifecycleOwner, Observer {
             adapter.setData(it)
@@ -45,5 +55,28 @@ class NoteListFragment : Fragment(){
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.list_menu, menu)
+        if(activity?.isDarkThemeOn() == true){
+            menu.findItem(R.id.menu_dark).isChecked = true
+        }else{
+            menu.findItem(R.id.menu_light).isChecked = true
+        }
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.menu_dark -> {
+                menuItem.isChecked = !menuItem.isChecked
+                if(menuItem.isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+            R.id.menu_light -> {
+                menuItem.isChecked = !menuItem.isChecked
+                if(menuItem.isChecked) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+        return true
     }
 }
